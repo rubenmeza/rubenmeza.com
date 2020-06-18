f50dc1acfeee0460dbeb1a8f5aa40f6676fc4179
Ruben Meza <ruben.meza@cubobit.com>
2020-06-18 14:41:08 -0500

    feat(web): landing v0.0.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="hero is-light is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <h1 class="title">Hey there!</h1>
+            <h1 class="title">I'm Ruben Meza</h1>
+            <h2 class="subtitle">FULL STACK DEVELOPER</h2>
+            <a href="https://www.linkedin.com/in/rubenmezarazo/" target="blank">
+              <i class="fab fa-linkedin-in"></i>
+            </a>
+            &nbsp;&nbsp;
+            <a href="https://gitlab.com/ruben.meza" target="blank">
+              <i class="fab fa-gitlab"></i>
+            </a>
+            &nbsp;&nbsp;
+            <a href="https://github.com/rubenmeza" target="blank">
+              <i class="fab fa-github"></i>
+            </a>
+            &nbsp;&nbsp;
+            <a href="https://twitter.com/_SoyUnPollo" target="blank">
+              <i class="fab fa-twitter"></i>
+            </a>
+            &nbsp;&nbsp;
+            <a href="mailto:rmezar@gmail.com" target="blank">
+              <i class="fas fa-at"></i>
+            </a>
+          </div>
+          <div class="column">
+            <h2 class="subtitle">Skills</h2>
+            <div class="tags">
+              <span class="tag is-white">nodejs</span>
+              <span class="tag is-white">golang</span>
+              <span class="tag is-white">vuejs</span>
+              <span class="tag is-white">git</span>
+              <span class="tag is-white">docker</span>
+              <span class="tag is-white">grpc</span>
+              <span class="tag is-white">micro services</span>
+              <span class="tag is-white">bitcoin</span>
+              <span class="tag is-white">blockchain</span>
+              <span class="tag is-white">firebase</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="hero-foot">
+      <div class="container has-text-centered">
+        <!-- <h2 class="subtitle"><i class="far fa-copyright"></i> Ruben Meza</h2> -->
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
