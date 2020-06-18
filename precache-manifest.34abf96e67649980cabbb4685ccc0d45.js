9b58fc9534e0ee142f6cd0d1e265b0dd6f34487e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-18 20:22:32 +0000

    Deploy rubenmeza.com to github.com/rubenmeza/rubenmeza.com.git:gh-pages

---
+++ precache-manifest.34abf96e67649980cabbb4685ccc0d45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb640b9b2672b6874dcb",
+    "url": "/css/index.ca936927.css"
+  },
+  {
+    "revision": "235a577e07a4eb4590552bb4c0789683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c678b97ab4dd2e06a3d",
+    "url": "/js/about.a1cec69b.js"
+  },
+  {
+    "revision": "63356ccd149785f0f87f",
+    "url": "/js/chunk-vendors.79b4cfa2.js"
+  },
+  {
+    "revision": "eb640b9b2672b6874dcb",
+    "url": "/js/index.d52c2441.js"
+  },
+  {
+    "revision": "4ccad91926a6e3abd713585ec10360bb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
